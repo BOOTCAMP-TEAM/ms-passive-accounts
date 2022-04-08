62009f70a5668b3aa66c9918b1f28f60ae280049
jhuayhuav <62222046+jhuayhuav@users.noreply.github.com>
2022-04-07 22:17:07 -0500

    jhuayhuav 07.04.22 Microservicios de Cuentas Pasivas

---
+++ src/main/java/com/bootcamp/passiveAccounts/MsPassiveAccountsApplication.java
@@ -0,0 +1,13 @@
+package com.bootcamp.passiveAccounts;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsPassiveAccountsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsPassiveAccountsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bootcamp/passiveAccounts/MsPassiveAccountsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bootcamp.passiveAccounts;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsPassiveAccountsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
